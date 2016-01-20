5d0c6b49ec7f6c85d4d5f1c6fd457165ae22f741
Louis-Francois Meunier <louis-francois.meunier@meteo.fr>
2016-01-20 16:04:49 +0100

    Increment the version number to prepare the next dev release

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ i.e. some set of keys/values pairs that attributes (possibly optionals) could co
 #: No automatic export
 __all__ = []
 
-__version__ = '0.9.25'
+__version__ = '0.9.26'
 
 import os
 import re
