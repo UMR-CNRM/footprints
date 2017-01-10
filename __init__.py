e2ff1abf6cb80ebaad6aa2f2b34a6ce145e32c2f
Louis-Francois Meunier <louis-francois.meunier@meteo.fr>
2017-01-10 16:51:56 +0100

    Increment the version number

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ i.e. some set of keys/values pairs that attributes (possibly optionals) could co
 #: No automatic export
 __all__ = []
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 import os
 import re
