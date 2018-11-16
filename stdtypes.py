e54e947e8d5958348ab36a843973af175a512ffe
Louis-Francois Meunier <louis-francois.meunier@meteo.fr>
2018-11-16 13:10:43 +0100

    Use of regex in footprint's only clauses for Target objects

--- stdtypes.py
+++ stdtypes.py
@@ -8,12 +8,11 @@ in order to avoid automatic value expansion (for example).
 
 from __future__ import print_function, absolute_import, division, unicode_literals
 
-from . import loggers
+import copy
+import re
 
 #: Automatic export
-__all__ = ['FPDict', 'FPList', 'FPSet', 'FPTuple']
-
-logger = loggers.getLogger(__name__)
+__all__ = ['FPDict', 'FPList', 'FPSet', 'FPTuple', 'FPRegex']
 
 
 class FPDict(dict):
@@ -54,3 +53,29 @@ class FPTuple(tuple):
     def items(self):
         """Return a list copy of internal components of the FPTuple."""
         return list(self)
+
+
+class FPRegex(object):
+    """A Compiled Regex like object that can be deepcopied"""
+
+    def __init__(self, pattern, flags=0):
+        self._re = re.compile(pattern, flags=flags)
+
+    def __getattr__(self, name):
+        return getattr(self._re, name)
+
+    def __deepcopy__(self, memo):
+        # A shallow copy is enough since self._re is not mutable
+        new = copy.copy(self)
+        memo[id(self)] = new
+        return new
+
+    def footprint_export(self):
+        return (self._re.pattern, self._re.flags)
+
+    def __str__(self):
+        return "FPRegex(r'{0:s}', flags={1:d})".format(self._re.pattern, self._re.flags)
+
+    def __repr__(self):
+        parent_repr = super(FPRegex, self).__repr__().rstrip('>')
+        return "{0:s} | {1:s}>".format(parent_repr, str(self))
