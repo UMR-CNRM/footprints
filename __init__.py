a843566775d90621303717d6fdb4c6d334e97b53
Louis-Francois Meunier <louis-francois.meunier@meteo.fr>
2019-01-15 09:39:17 +0100

    Increment version numbers

--- __init__.py
+++ __init__.py
@@ -28,7 +28,7 @@ from .stdtypes import *
 #: No automatic export
 __all__ = []
 
-__version__ = '1.5.1'
+__version__ = '1.6.0'
 
 __tocinfoline__ = 'A generic multi-purpose fabric for objects with tunable footprints'
 
